1025fd65df7df245a8a2372dbca065d864ee2e7f
Tran Hung <tranviethung912002@gmail.com>
2023-06-03 10:27:51 +0700

    .

---
+++ simple_mqtt.py
@@ -0,0 +1,21 @@
+#pip install adafruit-io
+import random
+import time
+import  sys
+from  Adafruit_IO import  MQTTClient
+
+
+AIO_USERNAME = "your_name"
+AIO_KEY = "your_key"
+
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+
+client.connect()
+client.loop_background()
+time.sleep(5)
+
+while True:
+    value = random.randint(0, 100)
+    client.publish("your_feed", value)
+    time.sleep(30)
